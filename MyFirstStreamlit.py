eb58a3ff195b230a6db1dc3b78c64c96d5fa9a65
Shahul-Snowpro <snowprosha@gmail.com>
2022-07-26 15:01:55 +0530

    Create MyFirstStreamlit.py

---
+++ MyFirstStreamlit.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import snowflake.connector as sc
+
+
+@st.experimental_singleton(suppress_st_warning=True)
+
+def init_connection():
+    return sc.connect(**st.secrets["snowflake"])
+
+conn = init_connection()
+
+@st.experimental_memo(ttl=600)
+
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+st.balloons()
+st.button('Click')
+# run_query("Select * from CARTRIDGE_COMPARE_TEMP;")
